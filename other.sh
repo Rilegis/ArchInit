e34350ce1f325502a176bf5707eb9eda515d8dc1
Rilegis <13032631+Rilegis@users.noreply.github.com>
2022-05-11 21:03:00 +0200

    Added potential utilities

--- other.sh
+++ other.sh
@@ -5,6 +5,13 @@ echo "[ARCHINIT] Installing useful packages..."
 pacman -S flatpak noto-fonts-emoji kicad rsync obs-studio
 # TBD switch 'obs-studio' to flatpak
 
+# Other utilities
+# piper (pacman) for gaming mouse configuration
+# openrgb (appimage) for rgb control
+# polymc/gdlauncher (flatpak) for MC
+# waydroid (aur) to run android apps on linux
+# kicad (pacman or flatpak) for pcb design
+
 # Virtualization
 echo "[ARCHINIT] Installing virtualization packages..."
 pacman -S libvirtd virt-manager qemu qemu-arch-extra
