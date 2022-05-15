94cdcf3aaa4493b6a1c497ed666b91b3bf001a29
Rilegis <13032631+Rilegis@users.noreply.github.com>
2022-05-15 16:56:31 +0200

    Added flatpak packages

--- other.sh
+++ other.sh
@@ -2,16 +2,21 @@
 
 # Useful packages
 echo "[ARCHINIT] Installing useful packages..."
-pacman -S flatpak noto-fonts-emoji kicad rsync obs-studio
-# TBD switch 'obs-studio' to flatpak
+pacman -S flatpak noto-fonts-emoji rsync
+
+# Flatpak utilities
+echo "[ARCHINIT] Installing useful flatpak packages..."
+flatpak install flathub org.gustavoperedo.FontDownloader # Font Downloader (Download and install fonts)
+flatpak install flathub fr.romainvigier.MetadataCleaner # Metadata Cleaner (Deletes metadata from files)
+flatpak install flathub org.polymc.PolyMC # PolyMC (FOSS fork of MultiMC, has better utilities and QOL improvements)
+#flatpak install flathub com.obsproject.Studio # OBS Studio (Not sure if i'll ever need it...but whatever)
+flatpak install --from https://flathub.org/repo/appstream/org.kicad.KiCad.flatpakref # KiCad (FOSS PCB design software)
 
 # Other utilities
 # piper (pacman) for gaming mouse configuration
 # openrgb (appimage) for rgb control
-# polymc/gdlauncher (flatpak) for MC
 # waydroid (aur) to run android apps on linux
-# kicad (pacman or flatpak) for pcb design
 
 # Virtualization
-echo "[ARCHINIT] Installing virtualization packages..."
-pacman -S libvirtd virt-manager qemu qemu-arch-extra
+#echo "[ARCHINIT] Installing virtualization packages..."
+#pacman -S libvirtd virt-manager qemu qemu-arch-extra
