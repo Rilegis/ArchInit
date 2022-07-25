d2dfcb16542787a096c98afad4cddd99d63a1a68
Rilegis <13032631+Rilegis@users.noreply.github.com>
2022-07-25 22:12:20 +0200

    Update
    Added AUR packages and LibreOffice flatpak installation.
    Converted system-wide flatpak installations to user-specific.

--- other.sh
+++ other.sh
@@ -8,14 +8,21 @@ pacman -S flatpak noto-fonts-emoji rsync grub-customizer
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si && cd ..
 
+# Install AUR packages
+yay -S appimagelauncher
+yay -S vscodium-git
+#yay -S gnome-browser-connector # Only for GNOME installs!
+
 # Flatpak utilities
 echo "[ARCHINIT] Installing useful flatpak packages..."
-flatpak install flathub org.gustavoperedo.FontDownloader # Font Downloader (Download and install fonts)
-flatpak install flathub fr.romainvigier.MetadataCleaner # Metadata Cleaner (Deletes metadata from files)
-flatpak install flathub org.polymc.PolyMC # PolyMC (FOSS fork of MultiMC, has better utilities and QOL improvements)
-#flatpak install flathub com.usebottles.bottles # Bottles (Run Windows software using wine)
-#flatpak install flathub com.obsproject.Studio # OBS Studio (Not sure if i'll ever need it...but whatever)
-#flatpak install --from https://flathub.org/repo/appstream/org.kicad.KiCad.flatpakref # KiCad (FOSS PCB design software)
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub --user org.gustavoperedo.FontDownloader # Font Downloader (Download and install fonts)
+flatpak install flathub --user fr.romainvigier.MetadataCleaner # Metadata Cleaner (Deletes metadata from files)
+flatpak install flathub --user org.polymc.PolyMC # PolyMC (FOSS fork of MultiMC, has better utilities and QOL improvements)
+flatpak install flathub --user com.usebottles.bottles # Bottles (Run Windows software using wine)
+#flatpak install flathub --user com.obsproject.Studio # OBS Studio (Not sure if i'll ever need it...but whatever)
+#flatpak install --from https://flathub.org/repo/appstream/org.kicad.KiCad.flatpakref --user # KiCad (FOSS PCB design software)
+flatpak install --from https://flathub.org/repo/appstream/org.libreoffice.LibreOffice.flatpakref --user # Office suite
 
 # Other utilities
 # piper (pacman) for gaming mouse configuration
