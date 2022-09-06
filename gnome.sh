fda33fb4e07f56c73e93892800b5c7340c8bdfcc
Rilegis <13032631+Rilegis@users.noreply.github.com>
2022-09-06 15:29:01 +0200

    Update gnome.sh
    Added packages:
      - 'eog' (Eye of GNOME, image viewer)
      - gnome-keyring
      - dconf-editor

--- gnome.sh
+++ gnome.sh
@@ -6,11 +6,11 @@ pacman -Syu
 
 # GNOME base installation (Includes Wayland compositor)
 echo "[ARCHINIT] Installing minimal GNOME setup..."
-pacman -S gnome-shell nautilus gnome-terminal gnome-software gnome-software-packagekit-plugin gnome-control-center gnome-disk-utility gnome-menus gnome-calculator gnome-screenshot gnome-user-share xdg-user-dirs-gtk xdg-desktop-portal-gnome gnome-keyring gdm
+pacman -S gnome-shell nautilus gnome-terminal gnome-software gnome-software-packagekit-plugin gnome-control-center gnome-disk-utility gnome-menus eog gnome-calculator gnome-screenshot gnome-user-share xdg-user-dirs-gtk xdg-desktop-portal-gnome gnome-keyring gdm
 
 # GNOME utilities installation
 echo "[ARCHINIT] Installing gnome utilities..."
-pacman -S gnome-tweak-tool gnome-backgrounds gnome-themes-extra gnome-shell-extensions gnome-remote-desktop gnome-connections network-manager-applet gnome-characters
+pacman -S gnome-tweak-tool gnome-backgrounds gnome-themes-extra gnome-shell-extensions gnome-remote-desktop gnome-connections network-manager-applet gnome-characters gnome-keyring dconf-editor
 
 # Enable GDM service
 echo "[ARCHINIT] Enabling GDM service..."
